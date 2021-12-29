efaf12aebdc57de13c94b00f121564669eca0f97
imightexist <94249457+imightexist@users.noreply.github.com>
2021-12-29 11:47:48 -0600

    Update typeinpass.js

--- typeinpass.js
+++ typeinpass.js
@@ -2,4 +2,5 @@ var targLink    = document.getElementById ("chat-user");
 var clickEvent  = document.createEvent ('MouseEvents');
 clickEvent.initEvent ('dblclick', true, true);
 targLink.dispatchEvent (clickEvent);
-// not copied code
+/* not copied code
+didnt work lol */
