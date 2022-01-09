430e4f16e87330ffed60a537691c1605803dfd74
Unzor <71789103+Unzor@users.noreply.github.com>
2022-01-08 18:34:34 -0600

    Update typeinpass.js

--- typeinpass.js
+++ typeinpass.js
@@ -1,6 +1,1 @@
-var targLink    = document.getElementById ("chat-user");
-var clickEvent  = document.createEvent ('MouseEvents');
-clickEvent.initEvent ('dblclick', true, true);
-targLink.dispatchEvent (clickEvent);
-/* not copied code
-didnt work lol */
+document.getElementById("chat-user").click();document.getElementById("chat-user").click();
